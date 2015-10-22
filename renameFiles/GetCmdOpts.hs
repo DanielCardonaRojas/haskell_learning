1597413c88d9ca118f2dfb0e3aac5638802a99cd
DanielCardonaRojas <d.cardona.rojas@gmail.com>
2015-10-21 23:08:39 -0500

    getArg for Selection

--- renameFiles/GetCmdOpts.hs
+++ renameFiles/GetCmdOpts.hs
@@ -1,35 +1,38 @@
 module GetCmdOpts ( 
-	renamingOpts,
-	Flag (..)
-	 ) where
+    renamingOpts,
+    Flag (..),
+    makeReplace
+     ) where
 
 import System.Environment ( getArgs )
 import System.Console.GetOpt
 import Data.Maybe( fromMaybe )
 import Text.Read
+import Data.List
 
--- | Get command line argumentes to build a Flag type.
+
 renamingOpts = do
   args <- getArgs
-  case getOpt RequireOrder options args of
+  case getOpt RequireOrder opciones args of
     (flags, [],      [])     -> return flags
     (_,     nonOpts, [])     -> error $ "unrecognized arguments: " ++ unwords nonOpts
-    (_,     _,       msgs)   -> error $ concat msgs ++ usageInfo header options
+    (_,     _,       msgs)   -> error $ concat msgs ++ usageInfo header' opciones
 
 --Add an options to replace String by another String
 data Flag =   TrimBeg Int  
-			| TrimEnd Int 
-			| Enumerate
-			| EnumerateBeg
-			| Delete String
-			| EnumPrepending String 
-			| EnumAppending String
-            | EnumWith String
-			| Clean 
-			| Append String
-			| Prepend String
-			| Replace String String
-			deriving (Show,Eq)
+            | TrimEnd Int 
+            | ListUsing String
+            | Enumerate
+            | EnumerateBeg
+            | Delete String
+            | EnumPrepending String 
+            | EnumAppending String
+            | Clean 
+            | Append String
+            | Prepend String
+            | Replace String String
+            | FileSelection String
+            deriving (Show,Eq)
 
 options :: [OptDescr Flag]
 options = [
@@ -43,11 +46,12 @@ options = [
     Option ['E'] ["enumApp"]   (ReqArg EnumAppending "String") "String that gets appended to number",
     Option ['n'] ["number"]   (NoArg Enumerate) "Enumerate file names at end",
     Option ['N'] ["numberBeg"]   (NoArg EnumerateBeg) "Enumerate file names at beginning",
-    Option ['l'] ["enumWith"]   (ReqArg EnumWith "String") "All files will have this name plus an extra number at the end",
-    Option ['r'] ["replace"]   (ReqArg makeReplace "oldString|newString") "Replace old substring with new string"	
+    Option ['l'] ["listWith"]   (ReqArg ListUsing "String") "Enumerate files prepending a constant string",
+    Option ['r'] ["replace"]   (ReqArg makeReplace "oldString|newString") "Replace old substring with new string",
+    Option ['s'] ["selection"]   (ReqArg FileSelection "String") "Choose a file extension or file name substring"
+
   ]
  
-
 opciones :: [OptDescr Flag]
 opciones = [
     Option ['c'] ["clean"] (NoArg Clean) "remueve characteres especiales, pone en minuscular y agregar guion bajo",
@@ -60,16 +64,20 @@ opciones = [
     Option ['E'] ["enumApp"]   (ReqArg EnumAppending "String") "Enumerar posponiendo una palabra",
     Option ['n'] ["number"]   (NoArg Enumerate) "Enumerar nombre con numero al final del nombre",
     Option ['N'] ["numberBeg"]   (NoArg EnumerateBeg) "Enumerar nombre con numero al principio del nombre",
-    -- Option ['l'] ["listWith"]   (ReqArg ListUsing "String") "Enumerar y remplazar nombre por palabra",
-    Option ['r'] ["replace"]   (ReqArg makeReplace "oldString|newString") "Reemplazar palabra vieja por nueva"
+    Option ['l'] ["listWith"]   (ReqArg ListUsing "String") "Enumerar y remplazar nombre por palabra",
+    Option ['r'] ["replace"]   (ReqArg makeReplace "oldString|newString") "Reemplazar palabra vieja por nueva",
+    Option ['s'] ["seleccion"]   (ReqArg FileSelection "String") "Elige una extension o  sub nombre"
   ]
 
 -- one possibility for handling optional file args:
 -- if no file is provided as argument, read from stdin
 
-tupleSepBy c str = let (f,s) = break (== c) str in (f, tail s)
-makeReplace = uncurry Replace . (tupleSepBy '|')
+tupleSepBy cs = break' (flip elem cs)
 
+break' p str = let (f,s) = break p str in (f, tail s)
 
-header = "Usage: main [OPTION...]"
+makeReplace = twoArgs Replace
+twoArgs f = uncurry f . (tupleSepBy "| ")
+
+header = "Ways to use this program..."
 header' = "Formas de uso de este programa..."
